1a86766a87a63a2f6f7870ac10ff7b240e78cf7a
alex <ghost.alex86@gmail.com>
2017-09-04 22:06:01 +0700

    y

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MdButtonModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
