271f36174b737e04883a070dfb725b218a0df728
aosivt <aosivt@gmail.com>
2017-09-05 13:59:06 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,29 @@ import { NgModule } from '@angular/core';
 import {DataSource} from '@angular/cdk/collections';
 import {Observable} from 'rxjs/Observable';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import {MdButtonModule,MdTableModule, MdInputModule} from '@angular/material';
+import {MdButtonModule,MdTableModule, MdInputModule,MdSortModule,} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { TableBasicExample } from './tab/tab.component';
 import { TableFilteringExample } from './tabFilter/tableFilteringExample.component';
+import { TableSortingExample } from './tabSort/tableSort.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableFilteringExample,
+    TableSortingExample,
     TableBasicExample
   ],
   imports: [
     BrowserModule,
     MdButtonModule,
-     MdInputModule,
+    MdInputModule,
+    MdSortModule,
     MdTableModule
 
   ],
   providers: [],
-  bootstrap: [AppComponent,TableBasicExample,TableFilteringExample]
+  bootstrap: [AppComponent,TableBasicExample,TableFilteringExample,TableSortingExample]
 })
 export class AppModule { }
