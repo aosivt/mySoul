13480d17831e2c47d4fe9f62cd35aa175319b1ff
aosivt <aosivt@gmail.com>
2017-09-07 10:20:06 +0700

    init

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -3,7 +3,7 @@ import {MdDialog, MdDialogRef, MD_DIALOG_DATA} from '@angular/material';
 import { Jsonp, URLSearchParams, Http, Response,Headers, RequestOptions } from '@angular/http';
 import { DataSource } from '@angular/cdk/collections';
 import { Observable } from 'rxjs/Observable';
-// import { TestService }                from "./serv/test.service";
+import { TestService }                from "./serv/test.service";
 
 
 import 'rxjs/add/observable/of';
@@ -23,39 +23,41 @@ export class DialogOverviewExample {
   animal: string;
   name: string;
 
-  constructor(public dialog: MdDialog
-    // ,  private  testService: TestService
+  constructor(
+    // public dialog: MdDialog
+    // ,
+    private  testService: TestService
   ) {}
 
   openDialog(): void {
-    let dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-      width: '450px',
-      data: { name: this.name, animal: this.animal }
-    });
+    // let dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      // width: '450px',
+      // data: { name: this.name, animal: this.animal }
+    // });
 
-      // this.testService.getComments().subscribe(resultService=>{
-      // console.log(resultService);
-      // });
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      this.animal = result;
-    });
+      this.testService.getComments().subscribe(resultService=>{
+      console.log(resultService);
+      });
+    // dialogRef.afterClosed().subscribe(result => {
+      // console.log('The dialog was closed');
+      // this.animal = result;
+    // });
   }
 
 }
 
-@Component({
-  selector: 'dialog-overview-example-dialog',
-  templateUrl: 'dialog-overview-example-dialog.html',
-})
-export class DialogOverviewExampleDialog {
-
-  constructor(
-    public dialogRef: MdDialogRef<DialogOverviewExampleDialog>,
-    @Inject(MD_DIALOG_DATA) public data: any) { }
-
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
-
-}
+// @Component({
+//   selector: 'dialog-overview-example-dialog',
+//   templateUrl: 'dialog-overview-example-dialog.html',
+// })
+// export class DialogOverviewExampleDialog {
+//
+//   constructor(
+//     public dialogRef: MdDialogRef<DialogOverviewExampleDialog>,
+//     @Inject(MD_DIALOG_DATA) public data: any) { }
+//
+//   onNoClick(): void {
+//     this.dialogRef.close();
+//   }
+//
+// }
